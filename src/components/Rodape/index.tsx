f838ad2b1b5583dcf6288051ca2b417c2193ee94
Luis Henrique <122834024+luissshc29@users.noreply.github.com>
2024-07-21 22:11:04 -0300

    Update index.tsx

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,21 +1,41 @@
-import classNames from 'classnames'
-import styles from './Rodape.module.scss'
-import {AiOutlineInstagram, AiOutlineLinkedin, AiOutlineGithub} from 'react-icons/ai'
+import classNames from "classnames";
+import styles from "./Rodape.module.scss";
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
 
-export default function Rodape({fixado = false}: {fixado?: boolean}) {
-
-	return (
-		<footer className={classNames({
-			[styles.rodape]: !fixado, 
-			[styles.rodape__fixado]: fixado
-		})}>
-			<p>Feito por Luis Henrique |</p>
-			<div className={styles.rodape__links}>
-				<a href='https://www.instagram.com/luissshc_/'><AiOutlineInstagram /></a>
-				<a href='https://www.linkedin.com/in/luis-henrique-6a7425165/'><AiOutlineLinkedin /></a>
-				<a href='https://github.com/luissshc29'><AiOutlineGithub /></a>
-			</div>
-		</footer>
-  )
+export default function Rodape({ fixado = false }: { fixado?: boolean }) {
+  return (
+    <footer
+      className={classNames({
+        [styles.rodape]: !fixado,
+        [styles.rodape__fixado]: fixado,
+      })}
+    >
+      <div className={styles.rodape__texto}>
+        Feito por{" "}
+        <a href="https://luishenrique-dev.com.br/" target="_blank">
+          Luis Henrique <FiExternalLink />
+        </a>{" "}
+        |
+      </div>
+      <div className={styles.rodape__links}>
+        <a href="https://www.instagram.com/luissshc_/" target="_blank">
+          <AiOutlineInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/luis-henrique-6a7425165/"
+          target="_blank"
+        >
+          <AiOutlineLinkedin />
+        </a>
+        <a href="https://github.com/luissshc29" target="_blank">
+          <AiOutlineGithub />
+        </a>
+      </div>
+    </footer>
+  );
 }
-
